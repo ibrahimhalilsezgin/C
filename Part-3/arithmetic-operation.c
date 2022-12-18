1996a8870a1d220d6e223a30c48587a77e9dfc88
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-12-18 19:09:54 +0300

    Create arithmetic-operation.c

---
+++ Part-3/arithmetic-operation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x = 10;
+    int y = 5;
+    printf("%d\n", x - y);
+    printf("%d\n", x + y);
+    printf("%d\n", x * y);
+    printf("%d\n", x / y);
+    printf("%d", x % y);    
+
+    return 0;
+}
