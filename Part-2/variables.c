89afb00ba76a04f734a87da24c92f286be2569c8
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-12-18 17:41:13 +0300

    Create variables.c

---
+++ Part-2/variables.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main(){
+    int a = 42;
+
+    char b1 = 66;
+    char b2 = 'B';
+    float c1 = 4.2;
+    double d1 = 0.21;
+
+    printf("%d %c %10d %.1f %.2f %s \n", a, b1, b2, c1, d1, "Messi Kaybetti");
+
+
+    printf("%d byte\n", sizeof(char));
+    printf("%d byte\n", sizeof(int));
+    printf("%d byte\n", sizeof(short int));
+    printf("%d byte\n", sizeof(long int));
+    printf("%d byte\n", sizeof(float));
+    printf("%d byte\n", sizeof(double));
+
+
+}
+
