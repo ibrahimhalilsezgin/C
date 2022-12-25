c30cc6333dbb0d493451860814f73732193d5f39
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-12-25 22:38:33 +0300

    Create logical-operation.c

---
+++ Part-5/logical-operation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    int x = 5;
+    int y = 10;
+
+    bool islem = x < y; // 1 = true 0 = false
+    printf("%d\n", islem);
+
+    
+
+    return 0;
+}
