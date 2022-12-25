565b6e52131be60f4d2cf74967bf4dec52b54ac0
Kaz <82215788+Kazdev0@users.noreply.github.com>
2022-12-25 23:27:13 +0300

    Create switch.c

---
+++ Part-7/switch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+
+    switch(a){
+        case 60:
+            printf("60\n");
+            break;
+        default:
+            printf("nope\n");
+            break;
+    }
+}
